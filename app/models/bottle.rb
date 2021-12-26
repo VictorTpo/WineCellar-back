26490c4a7d415af8d38d99e19a533b7d95b00a51
Victor Thépaut <2571475+VictorTpo@users.noreply.github.com>
2021-12-26 20:27:39 +0100

    user can drink and refill (#16)

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -3,4 +3,6 @@
 class Bottle < ActiveRecord::Base
   validates :name, presence: true
   belongs_to :wine_cellar, inverse_of: :bottles
+
+  validates :counter, comparison: { greater_than_or_equal_to: 0 }
 end
--- app/views/bottles/_bottle.jbuilder
+++ app/views/bottles/_bottle.jbuilder
@@ -2,6 +2,7 @@
 
 json.id bottle.id
 json.name bottle.name
+json.counter bottle.counter
 json.wine_cellar do
   json.id bottle.wine_cellar.id
   json.name bottle.wine_cellar.name
